996007b0aaff0be60cbb4b81e4d2793c4a1d4560
Aidan McKay <aidanmckay@Expensive-as-shit.local>
2019-11-05 10:27:37 -0700

    Create SynonymList in Application and display synonyms

---
+++ src/components/SynonymList/SynonymList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id='synonym-list' v-if='this.synonyms.length > 0'>
+    <ul>
+      <li 
+      v-for='(synonym, index) in this.synonyms' 
+      v-bind:key='index'
+      @click.prevent = 'findSynonyms(synonym)'>{{synonym}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'synonymlist',
+  props: ['synonyms', 'findSynonyms']
+}
+</script>
